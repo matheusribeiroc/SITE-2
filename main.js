62e5138d6045e57cf8322f848323ffae9f077d0d
M.ribeiro <140640049+matheusribeiroc@users.noreply.github.com>
2025-06-11 08:12:09 -0300

    reh4h

--- main.js
+++ main.js
@@ -11,10 +11,10 @@ for (let i=0; i <botoes.length; i++){
     }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivol = new Date ("2026-02-01T12:00");
-const tempoObjetivo2 = new Date ("2026-02-01T00:00");
-const tempoObjetivo3 = new Date ("2027-02-01T12:00");
-const tempoObjetivo4 = new Date ("2125-03-01T12:12");
+const tempoObjetivol = new Date ("2026-02-01T00:00:00");
+const tempoObjetivo2 = new Date ("2026-02-01T00:00:00");
+const tempoObjetivo3 = new Date ("2027-02-01T12:00:00");
+const tempoObjetivo4 = new Date ("2125-03-01T12:00:12");
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 function atualizaCronometro (){
     for (let i = 0; i < contadores.length; i++){
